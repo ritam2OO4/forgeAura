5c98ec04e1534e5a8f054df549c59455cfe70b1b
Ritam <ritamdevs2004@gmail.com>
2025-07-23 21:50:59 +0530

    Integrate inngest for background job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
-import {getQueryClient,trpc} from '@/trpc/server';
-import {Suspense} from "react";
-import {HydrationBoundary} from "@tanstack/react-query";
-import Client from "./client"
-import {dehydrate} from "@tanstack/query-core";
-const Page = async ()=>{
-    const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.hello.queryOptions({text:"Ritam prefetch"}))
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+import React from "react";
+
+const Page = () => {
+    const trpc = useTRPC();
+
+    const invoke = useMutation({
+        ...trpc.invoke.mutationOptions(),
+        onSuccess: () => {
+            toast.success("Background Job started!!");
+        },
+    });
+
     return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<p>...Loading</p>}>
-                <Client/>
-            </Suspense>
-        </HydrationBoundary>
+        <div className="p-4">
+            <Button
+                disabled={invoke.isPending}
+                onClick={() => invoke.mutate({ text: "Ritam" })}
+            >
+                Invoke Background Job
+            </Button>
+        </div>
     );
-}
+};
 
 export default Page;
