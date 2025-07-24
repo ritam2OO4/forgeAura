ee3372ce58487a58d1ffcaceceb270c39e83c79c
Ritam <ritamdevs2004@gmail.com>
2025-07-24 14:08:35 +0530

    Integrate inngest AgentKit using gemini as model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
-import React from "react";
+import React, {useState} from "react";
+import {Input} from "@/components/ui/input";
 
 const Page = () => {
+    const [value,setValue]=useState("")
     const trpc = useTRPC();
 
     const invoke = useMutation({
@@ -18,9 +20,10 @@ const Page = () => {
 
     return (
         <div className="p-4">
+            <Input onChange={(e)=>(setValue(e.target.value))} />
             <Button
                 disabled={invoke.isPending}
-                onClick={() => invoke.mutate({ text: "Ritam" })}
+                onClick={() => invoke.mutate({ value: value })}
             >
                 Invoke Background Job
             </Button>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,33 @@
 import { inngest } from "./client";
+import { createAgent, gemini } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
     async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "10s");
-        await step.sleep("wait-a-moment", "5s");
-        await step.sleep("wait-a-moment", "10s");
+        const CodeAgent = createAgent({
+            name: "code-agent",
+            system: "You are a senior-level Next.js expert and React.js engineer. Your job is to assist with writing clean, maintainable, and scalable frontend code. You follow industry best practices, including code modularity, component reusability, clear naming conventions, and performance optimization.\n" +
+                "- Use latest features of Next.js (e.g., App Router, Server Actions, Edge Functions where appropriate).\n" +
+                "- Write production-ready code that is easy to maintain and extend.\n" +
+                "- Use TypeScript when possible and explain when it's useful.\n" +
+                "- Prioritize accessibility and responsive design.\n" +
+                "- Include clear and well-commented code snippets.\n" +
+                "\n" +
+                "Whenever given a task, you generate idiomatic React/Next.js code snippets using functional components, proper directory structure (e.g., `app/`, `components/`, `lib/`), and TailwindCSS if styling is needed.\n" +
+                "\n" +
+                "If possible, also explain *why* a solution is preferred over others.\n",
+            model: gemini({
+                model: "gemini-1.5-flash-8b",
+                apiKey: process.env.GOOGLE_GEMINI_KEY,
+            }),
+        });
 
-        return { message: `Hello ${event.data.email}!` };
-    },
+        const inputText = event.data?.value ?? "No input provided";
+        const { output } = await CodeAgent.run(
+            `Write the following snnipet:\n${inputText}`
+        );
+
+        return { output };
+    }
 );
