0aaba042bf7d1b9e6914c48eee9148ee4b32c78f
Ritam <ritamdevs2004@gmail.com>
2025-07-28 23:14:37 +0530

    docker and e2b sandbox setup

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -8,7 +8,7 @@ export const helloWorld = inngest.createFunction(
     { event: "test/hello.world" },
     async ({ event, step }) => {
       const sandboxId= await step.run("get-sandbox-id",async()=>{
-          const sandbox = await Sandbox.create("code-interpreter-v1")
+          const sandbox = await Sandbox.create("forgeaura-builders-aap")
          return sandbox.sandboxId
           }
       )
